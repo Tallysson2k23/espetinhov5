e1245667247b49350af1d399e9b1e962d9db9b0d
Tallysson2k23 <tallysson.bldd@gmail.com>
2026-03-02 11:13:08 -0300

    melhorias

--- app/views/layouts/header.php
+++ app/views/layouts/header.php
@@ -5,10 +5,12 @@
     <title>Espetinho Central - V5</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <!-- APENAS ISSO FOI ADICIONADO -->
+    <link rel="icon" type="image/x-icon" href="/espetinhov5/public/espetinho_central.ico">
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="/espetinhov5/public/assets/css/tema-escuro.css">
-
-
 </head>
 <body>
+
 <div class="container mt-4">
